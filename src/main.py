3bc9922a88feca9efa2640382eea6ad6b0daa1e1
zetoooo <zitong.wang@hotmail.com>
2014-12-30 13:39:08 -0500

    cleaner_main.py
    don t run it. it s just a cleaner version of what it could be.

--- src/main.py
+++ src/main.py
@@ -3,34 +3,93 @@ from pygame.locals import *
 from enum import Enum
 import classes
 from classes import animation_loop
+import json
 
 # set up pygame
 pygame.init()
-pygame.font.init()
 
-is_online = False
-COMIC_SANS = os.path.join('..','data', 'comic.ttf')
-myfont = pygame.font.Font(COMIC_SANS, 15)
 
-# set up the window
-screen = pygame.display.set_mode((1440, 900), 0, 32)
-classes.screen = screen
+class Game:
+    
+    def __init__(self, argdict):
+        #argdic is everything all the options set from the menu
+        self.set_fps = argdict["set_fps"]
+        self.is_online = argdict["is_online"]
+        self.screen_size = argdict["screen_size"]
+        self.setup_screen()
+        self.setup_font()
+        self.load_images()
+        #self.setup_player()
+        #self.setup_npcs()
+    
+    
+    def setup_screen(self):
+        self.window = pygame.display.set_mode(self.screen_size, 0, 32)
+        self.clock = pygame.clock.Clock()
+        
+        
+    def setup_font(self):
+        #access font with self.fonts["Comic Sans-15"].render...
+        self.fonts = []
+        _fonts = [ ("Comic Sans", os.path.join('..','data', 'comic.ttf'), 15) ]
+        for name, path, size in _fonts:
+          key = "%s-%s" % (name, size)
+          self.fonts[key] = pygame.font.Font(path, size)
+    
+    def load_images(self):
+        #load the images that needs to be loaded for the specific level.
+        #each level has a specific json file that gives the images to load
+        #the json file should look like: [ ["hp_container", "../data/gui/bar_container.png"], ..]
+        _images = json.loads(self.imagepath)
+        for key, path in _images:
+          self.images[key] = pygame.image.load(path)
+   
+   def run(self):
+       self.bool_window = true
+       self.bool_game = true
+       self.bool_pause = false
+       while self.bool_window:
+           
+         while self.bool_game:
+           self.game_event()
+           self.game_logic()
+           self.game_draw()
+           pygame.display.update()
+           self.clock.tick(self.set_fps) #makes the game run at 60 fps
+           
+         while self.bool_pause:
+           self.pause_event()
+           self.pause_draw()
+           pygame.display.update()
+           self.clock.tick(self.set_fps) #makes the game run at 60 fps
+    
+   def pause_game(self):
+       self.bool_game = false
+       self.bool_pause = true
+   
+   def unpause_game(self):
+       self.bool_game = true
+       self.bool_pause = false
+   
+   def exit_game(self):
+       self.bool_game = false
+       self.bool_pause = false
+       self.bool_window = false
+       
 
-hp_container = pygame.image.load(os.path.join('..','data', 'gui', 'bar_container.png'))
-hp_bar = pygame.image.load(os.path.join('..','data', 'gui', 'hp_bar.png'))
-mana_bar = pygame.image.load(os.path.join('..','data', 'gui', 'mana_bar.png'))
-square = pygame.image.load(os.path.join('..','data', 'gui', 'square.png'))
-enemy_bar_container = pygame.image.load(os.path.join('..','data', 'gui', 'enemy_bar_container.png'))
-enemy_bar = pygame.image.load(os.path.join('..','data', 'gui', 'enemy_bar_fill.png'))
-boss = pygame.image.load(os.path.join('..','data', 'sprites', 'bosses', 'boss.png'))
+game = Game(argdict)
+game.run()
 
-# set up the colors
+#not useful.
+'''# set up the colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
+'''
+#rendered on the way   
+'''
 HP = myfont.render("HP", 1, WHITE)
 MP = myfont.render("MP", 1, WHITE)
 LH = myfont.render("LH", 1, WHITE)
@@ -39,11 +98,15 @@ S1 = myfont.render("1", 1, WHITE)
 S2 = myfont.render("2", 1, WHITE)
 S3 = myfont.render("3", 1, WHITE)
 S4 = myfont.render("4", 1, WHITE)
-
+'''
+#loaded from the menu, before the stage begins. therefore everything is in argdict
+'''
 health_value = 0.5 * 450
 mana_value = 0.7 * 450
-
-# draw the white background onto the surface
+'''
+# what is this... this doesnt make any sense because screen is filled in black in the loop.
+#do this in a loop
+'''
 screen.fill(BLACK)
 screen.blit(hp_bar, (20,830), (10,10,health_value,50) )
 screen.blit(mana_bar, (970,830), (10,10,mana_value,50) )
@@ -66,23 +129,9 @@ screen.blit(S2, (740, 810))
 screen.blit(S3, (810, 810))
 screen.blit(S4, (880, 810))
 screen.blit(boss, (700, 300))
+'''
 
-
-
-# draw the window onto the screen
-pygame.display.update()
-
-player = classes.Player()
-focus = player
-
-pos = (50,50,50)
-
-State = Enum('State', 'playing menu paused')
-state = State.playing
-things_on_screen = [player]
-
-paused = False
-
+'''
 # run the game loop
 while True:
     screen.fill(BLACK)
@@ -119,5 +168,4 @@ while True:
                         pass
 
         focus.key_event(event)
-    pygame.display.update()
-
+    pygame.display.update()'''
